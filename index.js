82f46d34489e96e35323e3c74aca97dd533dc368
James Gary <gmarchesi888@gmail.com>
2019-01-27 13:33:16 -0500

    Added useNewUrlParser.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb://localhost/mongoose_basics",
+  { useNewUrlParser: true },
   err => {
     if (err) throw err;
     console.log("Successfully connected.");
